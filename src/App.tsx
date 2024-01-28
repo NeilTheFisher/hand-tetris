889d534bb6aaa2ee137fc218feb7dbc4b3d9792f
Neil Fisher <nfisher@summit-tech.ca>
2024-01-27 22:48:17 -0500

    Gestures working

--- src/App.tsx
+++ src/App.tsx
@@ -14,69 +14,60 @@ function useHands(
   canvasRef: React.RefObject<HTMLCanvasElement>,
   contextRef: React.MutableRefObject<CanvasRenderingContext2D | null>
 ) {
-  // const indexFingerRef = useRef<NormalizedLandmark>();
   const [indexFingerLandmark, setIndexFingerLandmark] =
     useState<NormalizedLandmark>();
 
   const [loaded, setLoaded] = useState(false);
 
+  const gestureRecognizerRef = useRef<GestureRecognizer>();
+
   useEffect(() => {
-    const video = inputVideoRef.current;
-    const canvas = canvasRef.current;
-
-    if (!video || !canvas) return;
-
-    contextRef.current = canvas.getContext("2d");
-
-    const sendToMediaPipe = async () => {
-      await hands.send({ image: video });
-      requestAnimationFrame(sendToMediaPipe);
-    };
-
     (async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
+      if (!inputVideoRef.current) return;
+
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+      );
+      gestureRecognizerRef.current = await GestureRecognizer.createFromOptions(
+        vision,
+        {
+          baseOptions: {
+            modelAssetPath:
+              // "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+              "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task",
+            delegate: "GPU",
+          },
+          runningMode: "VIDEO",
+          numHands: 1,
+        }
+      );
+
+      const stream = await window.navigator.mediaDevices.getUserMedia({
         video: {
           height: 1280,
           width: 720,
         },
       });
-      video.srcObject = stream;
-      sendToMediaPipe();
-    })();
+      inputVideoRef.current.srcObject = stream;
 
-    const hands = new Hands({
-      locateFile: (file) =>
-        `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${VERSION}/${file}`,
-    });
+      const getGesture = async () => {
+        if (!gestureRecognizerRef.current || !inputVideoRef.current) return;
 
-    hands.setOptions({
-      maxNumHands: 1,
-      modelComplexity: 1,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5,
-    });
+        const result = gestureRecognizerRef.current.recognizeForVideo(
+          inputVideoRef.current,
+          performance.now()
+        );
 
-    const onResults = (results: Results) => {
-      const canvas = canvasRef.current;
-      const context = contextRef.current;
-
-      if (!canvas || !context) return;
-
-      setLoaded(true);
-
-      context.save();
-
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(results.image, 0, 0, canvas.width, canvas.height);
-      if (results.multiHandLandmarks && results.multiHandedness) {
-        for (let i = 0; i < results.multiHandLandmarks.length; i++) {
-          const classification = results.multiHandedness[i];
-          // const isRightHand = classification.label === "Right";
-          const landmarks = results.multiHandLandmarks[i];
-
+        if (result.gestures[0]) {
+          for (const category of result.gestures[0]) {
+            console.log(category.categoryName);
+            // if (category.categoryName === "Closed_Fist") {
+            // }
+          }
+        }
+        for (const multiHandLandmarks of result.landmarks) {
           // set index finger
-          // indexFingerRef.current = landmarks[8];
-          const newIndexFingerLandmark = landmarks[8];
+          const newIndexFingerLandmark = multiHandLandmarks[8];
           if (
             newIndexFingerLandmark.x > 1 ||
             newIndexFingerLandmark.x < 0 ||
@@ -98,25 +89,14 @@ function useHands(
           //   radius: (data) => lerp(data.from!.z!, -0.15, 0.1, 10, 1),
           // });
         }
-      }
-
-      context.restore();
-    };
-
-    hands.onResults(onResults);
-
-    const loadedDataHandler = () => {
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-    };
-    video.addEventListener("loadeddata", loadedDataHandler);
-
-    return () => {
-      video?.removeEventListener("loadeddata", loadedDataHandler);
-
-      hands.close();
-    };
-  }, [inputVideoRef.current, canvasRef.current, contextRef.current, setLoaded]);
+        requestAnimationFrame(getGesture);
+      };
+      inputVideoRef.current.addEventListener("loadeddata", () => {
+        setLoaded(true);
+        getGesture();
+      });
+    })();
+  }, []);
 
   return { indexFingerLandmark, loaded };
 }
@@ -167,11 +147,14 @@ const HandsContainer = () => {
           ></div>
         )}
         <video
-          style={{ opacity: "0", position: "fixed" }}
+          // style={{ opacity: "0", position: "fixed" }}
           autoPlay
-          ref={inputVideoRef}
+          playsInline
+          ref={(elem) => {
+            inputVideoRef.current = elem!;
+          }}
         />
-        <canvas ref={canvasRef} />
+        {/* <canvas ref={canvasRef} /> */}
         {!loaded && (
           <div style={{}}>
             <div style={{}}></div>
